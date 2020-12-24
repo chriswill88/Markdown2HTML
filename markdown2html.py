ae7bea153a9bfdd4f55fb201ee39f906131c596e
chriswill88 <williechri79@gmail.com>
2020-12-24 00:14:31 -0500

    [ADD] all

---
+++ markdown2html.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""markdown2html - render markdown to html"""
+import sys
+import os
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+if __name__ == "__main__":
+    toks = sys.argv
+
+    if len(toks) < 2:
+        eprint("Usage: ./markdown2html.py README.md README.html")
+        exit(1)
+
+    filename = toks[1]
+    if not os.path.exists(filename):
+        eprint("Missing {}".format(filename))
+        exit(1)
+    exit(0)
